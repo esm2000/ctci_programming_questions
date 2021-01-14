cec2cd218cf102a5362bea50329107e5e3cc9f85
esm2000 <embazor@gmail.com>
2021-01-14 01:38:09 -0500

    implement algorithm that checks if two strings are permutations of each other

---
+++ chapter_3/checkPermutation.py
@@ -0,0 +1,39 @@
+def checkPermutation(s1, s2):
+
+    # if the two strings are not equal in length
+    # they can't be permutations
+    if len(s1) != len(s2):
+        return False 
+    
+    d = {}
+
+    # have a dictionary that keeps track of 
+    # the number of times letters appear in 
+    # the first word
+    for c in s1:
+        if c in d:
+            d[c] += 1
+        else:
+            d[c] = 1
+    
+    # remove letters that are accounted for in
+    # the second word
+    for c in s2:
+        if c not in d:
+            return False
+        
+        d[c] -= 1
+
+    # if the final count of any letters is not 0 
+    # the words are not permutations
+    for c in d:
+        if d[c] != 0:
+            return False
+    
+    return True
+
+# should result in TTFF
+print(checkPermutation('abc', 'abc'),
+      checkPermutation('abc', 'cba'),
+      checkPermutation('a', 'ab'), 
+      checkPermutation('abc', 'abe'))
