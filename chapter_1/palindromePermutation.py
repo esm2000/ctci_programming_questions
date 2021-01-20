7a50b84c130e0c7a8ac98066b8387136af0bacae
esm2000 <embazor@gmail.com>
2021-01-19 19:57:53 -0500

    add algorithm that checks if a string is a permutation of a palindrome

---
+++ chapter_1/palindromePermutation.py
@@ -0,0 +1,36 @@
+def palindromePermutation(s):
+
+    # make string lowercase and strip it 
+    # of all it's spaces 
+    s = s.lower().replace(' ', '')
+
+    d = {}
+
+    # count the letter occurences in a 
+    # dictionary (letter: count)
+    for l in s: 
+        if l in d: 
+            d[l] += 1
+        else:
+            d[l] = 1
+
+    # if the string is odd in length 
+    # allow one occurence of an odd count
+    if len(s) % 2 == 1:
+        flag = False
+        for l in d:
+            if d[l] % 2 == 1:
+                if flag: 
+                    return False 
+                flag = True 
+    # if the string is even in length 
+    # allow no occurences of an even count 
+    else: 
+        for l in d: 
+            print('%s: %d' % (l, d[l]))
+            if d[l] % 2 == 1:
+                return False 
+
+    return True 
+
+print(palindromePermutation('Tact Coa'))
