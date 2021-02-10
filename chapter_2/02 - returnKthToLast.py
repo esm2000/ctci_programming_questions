8585845d4ec503800d4144961b0e0d89aaf52a7d
esm2000 <embazor@gmail.com>
2021-02-09 22:23:07 -0500

    remove the kth to last element in a linked list

---
+++ chapter_2/02 - returnKthToLast.py
@@ -0,0 +1,37 @@
+# Return Kth to Last: Implement an algorithm to
+# find the kth to last element of a singly linked list. 
+
+import linkedList
+
+def returnKthToLast(linked_list, k):
+
+    curr = linked_list.head
+    length = 1
+
+    while curr:
+        curr = curr.next
+        if curr:
+            length += 1
+
+    j = length - k + 1
+
+    for i in range(j):
+        if i == 0:
+            curr = linked_list.head
+        else:
+            curr = curr.next
+    
+    return curr.data
+
+llist = linkedList.SLinkedList()
+llist.Atbeginning(5)
+llist.Atbeginning(4)
+llist.Atbeginning(3)
+llist.Atbeginning(2)
+llist.Atbeginning(1)
+llist.LListprint()
+
+k = 4
+ans = returnKthToLast(llist, k)
+print('')
+print('%dth to Last Element in Linked List is %d' % (k, ans))
