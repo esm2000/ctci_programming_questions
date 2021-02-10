9f6ef15561476b33b8be4249a659bd635cbff05c
esm2000 <embazor@gmail.com>
2021-02-09 21:22:24 -0500

    remove duplicates from a linked list

---
+++ chapter_2/01 - removeDups.py
@@ -0,0 +1,34 @@
+# Remove Dups: Write code to remove duplicates
+# from an unsorted linked list.
+
+import linkedList
+
+def removeDups(linkedList):
+    record = {}
+
+    curr = linkedList.head
+    prev = None 
+
+    while curr:
+        if curr.data in record: 
+            prev.next = curr.next
+            curr = curr.next
+            continue
+
+        record[curr.data] = 0
+        prev = curr
+        curr = curr.next
+
+llist = linkedList.SLinkedList()
+llist.Atbeginning(5)
+llist.Atbeginning(5)
+llist.Atbeginning(4)
+llist.Atbeginning(3)
+llist.Atbeginning(3)
+llist.Atbeginning(2)
+llist.LListprint()
+
+print('')
+removeDups(llist)
+llist.LListprint()
+
