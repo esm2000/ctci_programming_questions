46e82738b72490667359993aac6f065bbb59f0bb
esm2000 <embazor@gmail.com>
2021-02-09 22:44:45 -0500

    delete the middle node from a linked list

---
+++ chapter_2/03 - deleteMiddleNode.py
@@ -0,0 +1,53 @@
+import linkedList
+
+# Delete Middle Node: Implement an algorithm to delete 
+# a node in the middle (i.e., any node but
+# the first and last node, not necessarily the
+# exact middle) of a singly linked list,
+# given only access to that node. 
+
+def deleteMiddleNode(linkedlist):
+    
+    p1 = linkedlist.head
+    p2 = linkedlist.head
+
+    while p1.next:
+        p1 = p1.next
+        if p1.next is None:
+            continue
+        p1 = p1.next
+
+        p2_prev = p2
+        p2 = p2.next
+
+    p2_prev.next = p2.next
+
+print('Even length')
+llist = linkedList.SLinkedList()
+llist.Atbeginning('f')
+llist.Atbeginning('e')
+llist.Atbeginning('d')
+llist.Atbeginning('c')
+llist.Atbeginning('b')
+llist.Atbeginning('a')
+llist.LListprint()
+
+print('')
+deleteMiddleNode(llist)
+llist.LListprint()
+
+print('')
+print('Odd length')
+
+llist = linkedList.SLinkedList()
+llist.Atbeginning('e')
+llist.Atbeginning('d')
+llist.Atbeginning('c')
+llist.Atbeginning('b')
+llist.Atbeginning('a')
+llist.LListprint()
+
+print('')
+deleteMiddleNode(llist)
+llist.LListprint()
+        
